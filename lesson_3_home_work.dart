01bcb027edaf9b309a61247a52ada750e17fcd09
chikkaa <turgunbaevcyngyz1@gmail.com>
2023-05-20 14:24:07 +0300

    first commit

---
+++ lesson_3_home_work.dart
@@ -0,0 +1,48 @@
+void main () {
+int a = 2 ;
+int b = 2;
+int c = a + b ;
+print("$a + $b = $c");
+
+int s = 10 ;
+int d = 5;
+int j = s - d ;
+print("$s - $d = $j");
+
+int w = 16 ;
+int e = 3;
+int l = w * e ;
+print("$w * $e = $l");
+
+int q = 18 ;
+int r = 2;
+int t = q ~/ r ;
+print("$q / $r = $t");
+
+int k =12;
+int o =15;
+print(k == o);
+ print(k != o);
+ print(k > o);
+ print(k < o);
+ print(k >= o);
+ print(k <= o);
+
+
+ int p = 11343;
+ print(p.toDouble());
+ double m = 2147.12;
+ print(m.toInt());
+ print(p.compareTo(m));
+ print(p.isEven);
+ print(p.isOdd);
+ print(m.round());
+print(m.ceil());
+int g = 41434;
+ print(p >332 && p > 3214);
+ print(p > 341 || p > 531);
+
+
+
+
+}
